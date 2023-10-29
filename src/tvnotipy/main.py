3eed570c524e7d603b0b935c8ef528d89617dfa0
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2023-10-29 18:41:34 +0100

    format

--- src/tvnotipy/main.py
+++ src/tvnotipy/main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 import os
-import requests
-from bs4 import BeautifulSoup
 from pathlib import Path
+
+import dbus
+import requests
+import schedule
+from bs4 import BeautifulSoup
 
 wiki_pages = [
     "https://en.wikipedia.org/wiki/Severance_(TV_series)",
@@ -11,7 +14,6 @@ wiki_pages = [
 ]
 
 for url in wiki_pages:
-
     req_str: str = requests.get(url=url).text
     html = BeautifulSoup(req_str, "html.parser")
     info_table = html.find(name="table", attrs={"class": "infobox vevent"})
@@ -19,9 +21,11 @@ for url in wiki_pages:
 
     for tr in table_rows:
         if tr.text.startswith("No. of seasons"):
-
-            cache_dir = (f"{xdg_cache}/tvnotipy" if (xdg_cache := os.getenv("XDG_CACHE_HOME"))
-                          and os.path.isabs(xdg_cache) else Path.home().joinpath(".cache", "tvnotipy"))
+            cache_dir = (
+                f"{xdg_cache}/tvnotipy"
+                if (xdg_cache := os.getenv("XDG_CACHE_HOME")) and os.path.isabs(xdg_cache)
+                else Path.home().joinpath(".cache", "tvnotipy")
+            )
             title = url.split("/")[-1]
             cache_file = os.path.join(cache_dir, title)
 
@@ -33,7 +37,7 @@ for url in wiki_pages:
                 with open(cache_file) as file:
                     num_of_seasons_last = file.read()
 
-            num_of_seasons_current=tr.text.replace("No. of seasons", "")
+            num_of_seasons_current = tr.text.replace("No. of seasons", "")
 
             if int(num_of_seasons_current) > int(num_of_seasons_last):
                 print(f"new season for {title}")
