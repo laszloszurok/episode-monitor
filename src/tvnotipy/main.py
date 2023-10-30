6dc060a8316400253f15ccc341deeca399cb91a4
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2023-10-30 20:52:43 +0100

    make tvnotipy read wiki urls from config file instead of a variable

--- src/tvnotipy/main.py
+++ src/tvnotipy/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import time
 from datetime import datetime
 from pathlib import Path
@@ -10,20 +11,33 @@ import requests
 import schedule
 from bs4 import BeautifulSoup
 
-wiki_pages = [
-    "https://en.wikipedia.org/wiki/Severance_(TV_series)",
-    "https://en.wikipedia.org/wiki/What_We_Do_in_the_Shadows_(TV_series)",
-    "https://en.wikipedia.org/wiki/Stranger_Things",
-]
-
 cache_dir = (
     f"{xdg_cache}/tvnotipy"
     if (xdg_cache := os.getenv("XDG_CACHE_HOME")) and os.path.isabs(xdg_cache)
     else Path.home().joinpath(".cache", "tvnotipy")
 )
 
+config_dir = (
+    f"{xdg_config}/tvnotipy"
+    if (xdg_config := os.getenv("XDG_CONFIG_HOME")) and os.path.isabs(xdg_config)
+    else Path.home().joinpath(".config", "tvnotipy")
+)
+
+urls_file = os.path.join(config_dir, "urls")
+
+if os.path.isfile(urls_file):
+    with open(urls_file) as file:
+        urls = [line.strip() for line in file]
+    if len(urls) == 0:
+        print(f"{urls_file} is empty. The file should contain a list of Wikipedia urls of TV series, one per line.")
+        sys.exit(1)
+else:
+    print(f"{urls_file} is empty. The file should contain a list of Wikipedia urls of TV series, one per line.")
+    sys.exit(1)
+
+
 def job():
-    for url in wiki_pages:
+    for url in urls:
         req_str: str = requests.get(url=url).text
         html = BeautifulSoup(req_str, "html.parser")
         info_table = html.find(name="table", attrs={"class": "infobox vevent"})
@@ -54,7 +68,7 @@ schedule.every().day.do(job)
 while True:
     schedule.run_pending()
 
-    for url in wiki_pages:
+    for url in urls:
         title = url.split("/")[-1]
         cache_file = os.path.join(cache_dir, title)
         if os.path.isfile(cache_file):
