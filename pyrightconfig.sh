203c1ed7df6f95aeb6f58cb706e2379f9983001e
László Szurok <48314396+laszloszurok@users.noreply.github.com>
2023-10-31 15:08:11 +0100

    fix venv_path in pyrightconfig.sh

--- pyrightconfig.sh
+++ pyrightconfig.sh
@@ -7,9 +7,11 @@ fi
 
 if ! [ -f pyrightconfig.json ]; then
     echo "generating pyrightconfig.json"
-    venv_path="$(hatch env find)"
-    venv_name="$(basename "$venv_path")"
-    echo "{ \"venvPath\": \"$venv_path\", \"venv\": \"$venv_name\", \"executionEnvironments\": [{\"root\": \"src/$venv_name\"}]}" \
+    venv="$(hatch env find)"
+    venv_path="$(dirname "$venv")"
+    venv_name="$(basename "$venv")"
+    printf "%s\n  %s\n  %s\n  %s\n%s\n" \
+        "{" "\"venvPath\": \"$venv_path\"," "\"venv\": \"$venv_name\"," "\"executionEnvironments\": [{\"root\": \"src/$venv_name\"}]" "}" \
         > pyrightconfig.json
 else
     echo "pyrightconfig.json exists already"
